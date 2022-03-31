8e4c504b96f39d7367b2272e7c453dc8aee1ee92
zhengkang <444856589@qq.com>
2022-03-31 15:35:23 +0800

    Initial commit

---
+++ src/main/java/com/example/HiveConnectorApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiveConnectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HiveConnectorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hiveconnector/HiveConnectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hiveconnector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiveConnectorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
